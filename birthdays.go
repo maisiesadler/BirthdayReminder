a4f91d76a6f564c1ecbe5e468492903ff10d621e
Maisie Sadler <maisiesadler@Maisies-MacBook-Pro.local>
2018-05-13 16:26:09 +0100

    fix build

--- birthdays.go
+++ birthdays.go
@@ -1,11 +1,7 @@
 package birthday_reminder
 
 import (
-	"encoding/json"
-	"fmt"
 	"time"
-
-	"./persist"
 )
 
 type Birthday struct {
@@ -13,48 +9,48 @@ type Birthday struct {
 	Date time.Time
 }
 
-var birthdays = []Birthday{}
+// var birthdays = []Birthday{}
 
-func loadBirthdays() {
-	b, _ := persist.Get("birthdays")
-	byt := []byte(b)
-	json.Unmarshal(byt, &birthdays)
-}
+// func loadBirthdays() {
+// 	b, _ := persist.Get("birthdays")
+// 	byt := []byte(b)
+// 	json.Unmarshal(byt, &birthdays)
+// }
 
-func addBirthday(name string, day int, month int) {
-	t := time.Now()
-	date := time.Date(0, time.Month(month), day, 0, 0, 0, 0, t.Location())
+// func addBirthday(name string, day int, month int) {
+// 	t := time.Now()
+// 	date := time.Date(0, time.Month(month), day, 0, 0, 0, 0, t.Location())
 
-	b := Birthday{Name: name, Date: date}
-	birthdays = append(birthdays, b)
-	byt, _ := json.Marshal(birthdays)
-	persist.Add("birthdays", string(byt))
-}
+// 	b := Birthday{Name: name, Date: date}
+// 	birthdays = append(birthdays, b)
+// 	byt, _ := json.Marshal(birthdays)
+// 	persist.Add("birthdays", string(byt))
+// }
 
-func printBirthdays() {
-	byt, _ := json.Marshal(birthdays)
-	fmt.Printf("birthdays: %+v", string(byt))
-}
-func printBirthdaysInDays(days int) {
-	tB := birthdaysInDays(days)
-	byt, _ := json.Marshal(tB)
-	fmt.Printf("todays birthdays: %+v", string(byt))
-}
+// func printBirthdays() {
+// 	byt, _ := json.Marshal(birthdays)
+// 	fmt.Printf("birthdays: %+v", string(byt))
+// }
+// func printBirthdaysInDays(days int) {
+// 	tB := birthdaysInDays(days)
+// 	byt, _ := json.Marshal(tB)
+// 	fmt.Printf("todays birthdays: %+v", string(byt))
+// }
 
-func birthdaysInDays(days int) []Birthday {
-	t := time.Now()
-	for d := 0; d < days; d++ {
-		t = t.Add(time.Hour * 24)
-	}
-	todays := []Birthday{}
-	tMonth := t.Month()
-	tDay := t.Day()
-	for _, birthday := range birthdays {
-		bMonth := birthday.Date.Month()
-		bDay := birthday.Date.Day()
-		if bMonth == tMonth && bDay == tDay {
-			todays = append(todays, birthday)
-		}
-	}
-	return todays
-}
+// func birthdaysInDays(days int) []Birthday {
+// 	t := time.Now()
+// 	for d := 0; d < days; d++ {
+// 		t = t.Add(time.Hour * 24)
+// 	}
+// 	todays := []Birthday{}
+// 	tMonth := t.Month()
+// 	tDay := t.Day()
+// 	for _, birthday := range birthdays {
+// 		bMonth := birthday.Date.Month()
+// 		bDay := birthday.Date.Day()
+// 		if bMonth == tMonth && bDay == tDay {
+// 			todays = append(todays, birthday)
+// 		}
+// 	}
+// 	return todays
+// }
